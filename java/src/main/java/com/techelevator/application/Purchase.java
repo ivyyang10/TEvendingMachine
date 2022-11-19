9c0c1573a7e9a316d2712e2ac90d8c9e79f814cf
Xiaojie <ivyyangxiaoxiao@gmail.com>
2022-11-18 22:42:23 -0500

    finish selecting part

--- java/src/main/java/com/techelevator/application/Purchase.java
+++ java/src/main/java/com/techelevator/application/Purchase.java
@@ -12,14 +12,13 @@ public class Purchase {
         Products test2 = new Products();
         QuipOutput quipGrab = new QuipOutput();
         Scanner input = new Scanner(System.in);
-        BigDecimal totalInput = new BigDecimal("0");
-        BigDecimal remainingBalance = new BigDecimal("0");
+        BigDecimal Balance = new BigDecimal("0");
         boolean keepLooping = true;
-
-        UserInput.getPurchaseScreenOption();
 
 
         while (keepLooping) {
+            UserInput.getPurchaseScreenOption();
+
             String option = input.nextLine();
             if (option.equalsIgnoreCase("M")) {
                 boolean enter = true;
@@ -27,33 +26,33 @@ public class Purchase {
                     System.out.println("Please input whole dollar amounts: $1 , $5, $10, $20, please enter (E) when you finish input.");
                     String inputAmount = input.nextLine();
                     if (!inputAmount.equalsIgnoreCase("E")) {
-                        totalInput = totalInput.add(new BigDecimal(inputAmount));
-                        System.out.println(" Current Money Provided: $" + totalInput);
+                        Balance = Balance.add(new BigDecimal(inputAmount));
+                        System.out.println(" Current Money Provided: $" + Balance);
                     } else {
                         enter = false;
                     }
                 }
+
             }
 
-            UserInput.getPurchaseScreenOption();
-            System.out.println(" ");
 
-            if(option.equalsIgnoreCase("S")) {
-                System.out.println("Please enter a slot identifier to select an item:");
+            if (option.equalsIgnoreCase("S")) {
+                boolean isContinue=true;
+
                 Display.run();
-                while(keepLooping) {
+                while(isContinue) {
+                    System.out.println("Please enter a slot identifier to select an item:");
                     String userInput = input.nextLine().toUpperCase();
                     if (!test2.getMapOfItemAmount().containsKey(userInput)) {
                         //if the item doesn't exist returns the user to purchase menu.
                         System.out.println("Item not sold here. Please choose other item.");
-                        keepLooping = false;
-//                UserInput.getPurchaseScreenOption();
+
 
                     } else if (test2.getMapOfItemAmount().get(userInput) > 0) {
                         //adds item to cart
-                        if (remainingBalance.compareTo(test2.getMapOfItemPrice().get(userInput)) < 0) {
-                            remainingBalance = totalInput.subtract(test2.getMapOfItemPrice().get(userInput));
-                            System.out.println(test2.getMapOfItemNames().get(userInput) + ", " + test2.getMapOfItemPrice().get(userInput) + ", remaining balance $" + remainingBalance);
+                        if (Balance.compareTo(test2.getMapOfItemPrice().get(userInput)) > 0) {
+                            Balance = Balance.subtract(test2.getMapOfItemPrice().get(userInput));
+                            System.out.println(test2.getMapOfItemNames().get(userInput) + ", " + test2.getMapOfItemPrice().get(userInput) + ", remaining balance $" + Balance);
                             System.out.println(quipGrab.getQuip(test2.getMapOfItemType().get(userInput)));
                             test2.reduceMapOfItemAmount(userInput);
                         } else {
@@ -63,26 +62,25 @@ public class Purchase {
                     } else {
                         //tells the customer that the item is out of stock and returns to purchase menu
                         System.out.println("Stop trying to buy this item we are out of stock.");
-                        keepLooping = false;
 
                     }
-                    System.out.println("Do you want select more item? (Y)es. if Yes, please enter item slot number");
+                    System.out.println("Do you want select more item? (Y)es.");
                     String more = input.nextLine();
-                    String userInput2 = input.nextLine().toUpperCase();
                     if (more.equalsIgnoreCase("Y")) {
+                        System.out.println("Please enter item slot number.");
+                        String userInput2 = input.nextLine().toUpperCase();
                         if (!test2.getMapOfItemAmount().containsKey(userInput2)) {
                             //if the item doesn't exist returns the user to purchase menu.
                             System.out.println("Item not sold here. Please choose other item.");
-                            keepLooping = false;
-//                UserInput.getPurchaseScreenOption();
 
                         } else if (test2.getMapOfItemAmount().get(userInput2) > 0) {
                             //adds item to cart
-                            if (remainingBalance.compareTo(test2.getMapOfItemPrice().get(userInput2)) < 0) {
-                                remainingBalance = remainingBalance.subtract((test2.getMapOfItemPrice().get(userInput2)).divide(new BigDecimal("2")));
-                                System.out.println(test2.getMapOfItemNames().get(userInput2) + ", " + test2.getMapOfItemPrice().get(userInput2) + ", remaining balance $" + remainingBalance);
+                            if (Balance.compareTo(test2.getMapOfItemPrice().get(userInput2)) > 0) {
+                                Balance = Balance.subtract((test2.getMapOfItemPrice().get(userInput2).subtract(new BigDecimal("1"))));
+                                System.out.println(test2.getMapOfItemNames().get(userInput2) + ", " + test2.getMapOfItemPrice().get(userInput2) + ", remaining balance $" + Balance);
                                 System.out.println(quipGrab.getQuip(test2.getMapOfItemType().get(userInput2)));
                                 test2.reduceMapOfItemAmount(userInput2);
+
                             } else {
                                 System.out.println("Not enough money in the machine. Please input more money");
                             }
@@ -90,20 +88,29 @@ public class Purchase {
                         } else {
                             //tells the customer that the item is out of stock and returns to purchase menu
                             System.out.println("Stop trying to buy this item we are out of stock.");
-                            keepLooping = false;
 
                         }
-
-                    } else{
-                        keepLooping = false;
-
-                    }
+                        System.out.println("Do you want select more item? (Y)es.");
+                        String again = input.nextLine();
+                        if (!again.equalsIgnoreCase("Y")) {
+                            isContinue = false;
+                        }
+                    }else{ isContinue = false;}
                 }
+
             }
 
+
+
+            if (option.equalsIgnoreCase("F")) {
+
+                //Please edit this part. we only have this and audit file left.
+
+
+
+            }
 
         }
     }
 }
-
 
